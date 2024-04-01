4344c129949a2d1f92025ad19fa76956e0979e76
Epingle <73447649+AgrafeModel@users.noreply.github.com>
2024-04-01 21:51:39 +0200

    Delete build directory

--- build/CMakeFiles/make.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/make"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/make.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
